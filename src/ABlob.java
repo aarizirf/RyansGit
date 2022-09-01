db92b9d701c0f7feacc371909e9bd9eaf0d2f6db
Ryan Pinsker <cjumper555@gmail.com>
2022-09-01 10:26:55 -0700

    added sha-1 encryption

--- src/ABlob.java
+++ src/ABlob.java
@@ -1,12 +1,24 @@
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ABlob {
 	
-	
-	public static String encryptThisString(String input)
-    {
+	private static String getFileString(String fileName) throws IOException {
+		Path filePath = Path.of(fileName);
+		return Files.readString(filePath);
+	}
+
+	private static String encryptThisString(String input) {
         try {
             // getInstance() method is called with algorithm SHA-1
             MessageDigest md = MessageDigest.getInstance("SHA-1");
@@ -30,9 +42,46 @@ public class ABlob {
             // return the HashText
             return hashtext;
         }
- 
         // For specifying wrong message digest algorithms
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+    	
+}
+	private static void copyContent(File a, File b) throws Exception
+	    {
+	        FileInputStream in = new FileInputStream(a);
+	        FileOutputStream out = new FileOutputStream(b);
+	  
+	        try {
+	  
+	            int n;
+	  
+	            // read() function to read the
+	            // byte of data
+	            while ((n = in.read()) != -1) {
+	                // write() function to write
+	                // the byte of data
+	                out.write(n);
+	            }
+	        }
+	        finally {
+	            if (in != null) {
+	  
+	                // close() function to close the
+	                // stream
+	                in.close();
+	            }
+	            // close() function to close
+	            // the stream
+	            if (out != null) {
+	                out.close();
+	            }
+	        }
+	        System.out.println("File Copied");
+	    }
+	public static void main (String[] args) {
+		
+	}
+	
 }
--- src/ABlobTester.java
+++ src/ABlobTester.java
@@ -1,4 +1,9 @@
-
+package testers;
+import java.io.IOException;
+import git.Blob;
 public class ABlobTester {
-
+	public static void main(String[] args) throws IOException{
+		Blob blob = new Blob(".\\test\\.txt");
+		System.out.println(encryptThisString(blob));
+	}
 }
