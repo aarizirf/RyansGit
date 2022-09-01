633cc454aaeb1efee07c149690547aa9b3a7ce49
Ryan Pinsker <cjumper555@gmail.com>
2022-09-01 12:57:00 -0700

    added getFileString and copyContent functoins

--- src/ABlob.java
+++ src/ABlob.java
@@ -1,20 +1,19 @@
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ABlob {
-	
+	//Takes in file name and returns string of file contents
 	private static String getFileString(String fileName) throws IOException {
+		//finds file path
 		Path filePath = Path.of(fileName);
+		//gets file content from file path
 		return Files.readString(filePath);
 	}
 
@@ -22,15 +21,12 @@ public class ABlob {
         try {
             // getInstance() method is called with algorithm SHA-1
             MessageDigest md = MessageDigest.getInstance("SHA-1");
- 
             // digest() method is called
             // to calculate message digest of the input string
             // returned as array of byte
             byte[] messageDigest = md.digest(input.getBytes());
- 
             // Convert byte array into signum representation
             BigInteger no = new BigInteger(1, messageDigest);
- 
             // Convert message digest into hex value
             String hashtext = no.toString(16);
  
@@ -38,7 +34,6 @@ public class ABlob {
             while (hashtext.length() < 32) {
                 hashtext = "0" + hashtext;
             }
- 
             // return the HashText
             return hashtext;
         }
@@ -46,17 +41,13 @@ public class ABlob {
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
-    	
 }
 	private static void copyContent(File a, File b) throws Exception
 	    {
 	        FileInputStream in = new FileInputStream(a);
 	        FileOutputStream out = new FileOutputStream(b);
-	  
 	        try {
-	  
 	            int n;
-	  
 	            // read() function to read the
 	            // byte of data
 	            while ((n = in.read()) != -1) {
@@ -67,7 +58,6 @@ public class ABlob {
 	        }
 	        finally {
 	            if (in != null) {
-	  
 	                // close() function to close the
 	                // stream
 	                in.close();
@@ -80,8 +70,10 @@ public class ABlob {
 	        }
 	        System.out.println("File Copied");
 	    }
+	//TODO: 1) Make function to create the file into the objects folder in the Git directory function to write the file. 
+	//TODO: 2) Make constructor that combines these methods and actually makes the Blob
 	public static void main (String[] args) {
-		
+		System.out.println(encryptThisString("Hello World"));
 	}
 	
 }
